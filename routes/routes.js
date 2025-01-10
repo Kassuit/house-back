534e893e09e263f4e038109772facf641e55b681
Gabucado <awsalinas@uc.cl>
2025-01-09 22:44:01 -0300

    🛣️ Routes: Se exponen like y followers

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,14 @@
 const Router = require("koa-router");
 const user_routes = require("./user.js");
 const post_routes = require('./post.js');
+const like_routes = require('./like.js')
+const foll_routes = require('./follower.js')
 
 const router = new Router();
 
 router.use(user_routes.routes());
 router.use(post_routes.routes());
+router.use(like_routes.routes());
+router.use(foll_routes.routes());
 
 module.exports = router;
