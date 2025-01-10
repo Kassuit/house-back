9d009995f5803bc66aa27561c338e119d7bd2843
Gabucado <awsalinas@uc.cl>
2025-01-09 22:42:13 -0300

    🛣️ Routes: Se exponen solo user y post

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 const Router = require("koa-router");
 const user_routes = require("./user.js");
+const post_routes = require('./post.js');
 
 const router = new Router();
 
 router.use(user_routes.routes());
+router.use(post_routes.routes());
 
 module.exports = router;
